9fd4d82c14cebc1f1843391f4925450f99af39d9
eldardamirov <eldardamirov75@mail.ru>
2018-04-26 11:24:07 +0300

    Initial Commit

---
+++ Calculus One/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculus One
+//
+//  Created by Эльдар Дамиров on 26.04.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
